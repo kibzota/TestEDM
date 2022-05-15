c00c398e722d26d4a151daa6680d88f23d493f2a
Danilo Soares <danilo3457@gmail.com>
2022-05-15 14:38:18 -0300

    criado primeiro migration

---
+++ TestEDM/TestEDM.Infra/Migrations/20220515162430_migration01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestEDM.Infra.Migrations
+{
+    public partial class migration01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NotasDeDez = table.Column<int>(type: "int", nullable: false),
+                    NotasDeVinte = table.Column<int>(type: "int", nullable: false),
+                    NotasDeCinquenta = table.Column<int>(type: "int", nullable: false),
+                    NotasDeCem = table.Column<int>(type: "int", nullable: false),
+                    DataAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notas");
+        }
+    }
+}
