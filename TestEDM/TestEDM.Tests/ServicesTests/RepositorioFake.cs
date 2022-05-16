71ec56fd70bad975061cccccb97db26587762663
Danilo Soares <danilo3457@gmail.com>
2022-05-16 15:39:02 -0300

    adicionado teste

---
+++ TestEDM/TestEDM.Tests/ServicesTests/RepositorioFake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestEDM.Domain.Entities;
+using TestEDM.Domain.Interfaces;
+
+namespace TestEDM.Tests.ServicesTests
+{
+    class RepositorioFake : INotasRepository
+    {
+        public Notas? NotasDisponiveis()
+        {
+            return new Notas()
+            {
+                NotasDeCem = 2,
+                NotasDeCinquenta = 0,
+                NotasDeVinte = 0,
+                NotasDeDez = 30,
+            };
+        }
+
+        public void SalvarNotas(Notas notas)
+        {
+            
+        }
+    }
+}
---
+++ TestEDM/TestEDM.Tests/ServicesTests/SacarNotasServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using TestEDM.Domain.Entities;
+using TestEDM.Domain.Services;
+using TestEDM.Shared.Exceptions;
+using Xunit;
+
+namespace TestEDM.Tests.ServicesTests
+{
+    
+    public class SacarNotasServiceTest
+    {
+        [Fact]
+        public void SacarNotasDeveRetornarValorCorreto()
+        {
+            var valorRequirido = 400;
+
+            var repo = new RepositorioFake();
+            var service = new SacarNotasService(repo);
+
+            var notasRetiradas = service.Saque(valorRequirido);
+
+            Assert.Equal(2,notasRetiradas.NotasDeCem);
+            Assert.Equal(0, notasRetiradas.NotasDeCinquenta);
+            Assert.Equal(0, notasRetiradas.NotasDeVinte);
+            Assert.Equal(20, notasRetiradas.NotasDeDez);
+
+        }
+        [Fact]
+        public void SacarNotasDeveRetornarException()
+        {
+            var valorRequirido = 40000;
+
+            var repo = new RepositorioFake();
+            var service = new SacarNotasService(repo);
+
+            var ex = Assert.Throws<SaldoCaixaException>(() => service.Saque(valorRequirido));
+
+            Assert.IsType<SaldoCaixaException>(ex);
+
+        }
+    }
+}
